24eba1a593e6ccca025fca56f7a4ee61d48a1eaa
fahim-ahmed-ifty <fahimahmediftyzettabyte@gmail.com>
2025-02-16 12:47:40 +0600

    ✅repos page fetch data

---
+++ app/about/team/page.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+export const metadata = {
+	title: 'About Team'
+};
+const TeamPage = () => {
+	return (
+		<div>
+			<h1 className='text-3xl font-bold mb-2'>Team Page</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum
+				sit, minus minima accusantium quae totam dignissimos, quam, et
+				veniam a impedit eius officia quidem vitae perspiciatis
+				obcaecati inventore dolores reiciendis. Lorem ipsum dolor sit
+				amet consectetur adipisicing elit. Cum recusandae nemo nobis
+				maxime magnam nihil quaerat necessitatibus enim, perferendis
+				est obcaecati perspiciatis voluptatem asperiores aliquid
+				nesciunt praesentium ipsam a alias?
+			</p>
+		</div>
+	);
+};
+
+export default TeamPage;
