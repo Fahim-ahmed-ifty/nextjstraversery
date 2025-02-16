cf0ea5f50e2d72c39cd469ac2e4c05dba5daef3a
fahim-ahmed-ifty <fahimahmediftyzettabyte@gmail.com>
2025-02-16 12:49:57 +0600

    ✅repos page fetch data

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,10 @@
-import Link from 'next/link';
 import React from 'react';
 
 const HomePage = () => {
 	return (
 		<div>
 			<h1>Welcome to ZettaByte Media</h1>
-			<ul>
-				<li className='hover:text-blue-300'>
-					<Link href='/'>Home</Link>
-				</li>
-				<li className='hover:text-blue-300'>
-					<Link href='/about'>About</Link>
-				</li>
-				<li className='hover:text-blue-300'>
-					<Link href='/about/team'>Team</Link>
-				</li>
-			</ul>
+
 		</div>
 	);
 };
